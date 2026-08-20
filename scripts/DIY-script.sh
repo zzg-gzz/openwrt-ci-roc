b4d6b5ea51b7c5989230ff148ee2ccaf2fb9efdf
zzg-gzz <zzggood@126.com>
2026-08-20 14:25:08 +0800

    Comment out zsh default shell change in DIY-script
    Commented out the command to change the default shell to zsh and added a note about potential login issues if zsh is not installed.

--- scripts/DIY-script.sh
+++ scripts/DIY-script.sh
@@ -31,8 +31,8 @@ sed -i "s#_('Firmware Version'), (L\.isObject(boardinfo\.release) ? boardinfo\.r
                 }, [ 'Built by Roc $(date "+%Y-%m-%d %H:%M:%S")' ])\n \
             ]),#" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
 
-# 更改默认 Shell 为 zsh
-sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
+# 更改默认 Shell 为 zsh 未安装zsh会导致SSH、SCP无法登录
+# sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # TTYD 免登录
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
